2fef5a6ec2f7e479e38dd30046a90c074828ffd8
Andrea Riccardi <andrea.riccardi03@gmail.com>
2023-09-09 16:14:47 +0200

    Initial commit

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,20 @@
+export type AmplifyDependentResourcesAttributes = {
+  "analytics": {
+    "icecream": {
+      "Id": "string",
+      "Region": "string",
+      "appName": "string"
+    }
+  },
+  "auth": {
+    "icecream043fc528": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
